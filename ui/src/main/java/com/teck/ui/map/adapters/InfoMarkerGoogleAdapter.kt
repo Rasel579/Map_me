1ea3752311c6aaab3b9bc46cb16c88caf58741fd
Rasel579 <Rasel579@gmail.com>
2022-02-09 01:06:13 +0500

    1. Добавлена возможность добавлять маркеры 2. Добавлено нахождение своего местоположения 3. Добавлены Permissions 4. GoogleMaps сокрыта под абстракцией Map и реализована в MapImpl.kt

---
+++ ui/src/main/java/com/teck/ui/map/adapters/InfoMarkerGoogleAdapter.kt
@@ -0,0 +1,24 @@
+package com.teck.ui.map.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.model.Marker
+import com.teck.domain.models.Place
+import com.teck.ui.databinding.MarkerInfoLayoutBinding
+
+class InfoMarkerGoogleAdapter(
+    private val context: Context
+) : GoogleMap.InfoWindowAdapter {
+    private var viewBinding: MarkerInfoLayoutBinding? = null
+    override fun getInfoContents(marker: Marker): View? {
+        val place = marker.tag as? Place?
+        viewBinding = MarkerInfoLayoutBinding.inflate(LayoutInflater.from(context))
+        viewBinding?.titleTextview?.text = place?.name
+        viewBinding?.adressTextview?.text = place?.address
+        viewBinding?.ratingTextview?.text = "Rating: ${place?.rating.toString()}"
+        return viewBinding?.root
+    }
+    override fun getInfoWindow(marker: Marker): View? = null
+}
